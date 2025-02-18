be4e96d1febbeb639fd33ece16874360a5f1d88c
IL-HH <ismailhamdach@gmail.com>
2025-02-18 22:57:55 +0100

    missing checkout filling with start-end time number of hours for each employee

--- Jobs/app.js
+++ Jobs/app.js
@@ -14,16 +14,19 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env.local") }); //
     console.log("Connected successfully")
     try {
         console.log("Fetching logs data...")
-        const [rows] = await db.query("SELECT * FROM attendance_logs WHERE check_out is null AND TIMESTAMPDIFF(HOUR, check_in, NOW()) >= 14; ");
+        const [rows] = await db.query("SELECT * FROM attendance_logs a JOIN employees e ON e.user_id = a.user_id JOIN schedules s ON s.employee_id = e.user_id JOIN shifts sh ON sh.shift_id = s.shift_id WHERE check_out IS NULL AND TIMESTAMPDIFF(HOUR, check_in, NOW()) >= 14;");
         console.log("Logs data fetched successfully")
         if (rows.length > 0) {
             console.log(rows.length + " missing checkout data has found.")
             console.log("Logs data updating ...")
             for (const row of rows) {
                 console.log("updating " + row.id);
+                const startTime = new Date(`1970-01-01T${row.start_time}`); // Fixed start time
+                const endTime = new Date(`1970-01-01T${row.end_time}`); // Fixed end time
+                const nbrHours = (endTime - startTime) / (1000 * 60 * 60); // Calculate hours
                 await db.query(
                     `UPDATE attendance_logs 
-                    SET check_out = DATE_ADD(check_in, INTERVAL 8 HOUR) 
+                    SET check_out = DATE_ADD(check_in, INTERVAL ${nbrHours} HOUR) 
                     WHERE id = ${row.id};`
                 )
                 console.log("Updated " + row.id);
